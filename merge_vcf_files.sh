6f5041b152f27beab787be9a1f0dea501a84b2d9
Cong <42413479+MarchOnion@users.noreply.github.com>
2022-10-05 19:57:35 -0400

    Update merge_vcf_files.sh

--- merge_vcf_files.sh
+++ merge_vcf_files.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
-
 /bcftools-1.16/bcftools merge \
   --missing-to-ref \
   --filter-logic + \
   --merge none \
-  --file-list het_16930.vcf.list \
-  --output    P1_MT17K.mutect2.HETat5-95percent.vcf.gz \
+  --file-list vcf.list.txt \
+  --output    merged.vcf.gz \
   --output-type z \
